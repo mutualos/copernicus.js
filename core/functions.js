86bfb9ddbfdcee3efc973ec727846365ec71b124
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-14 21:32:29 -0400

    Update functions.js
    initial commit

--- core/functions.js
+++ core/functions.js
@@ -1,5 +1,12 @@
-const fiFunctions = {   
-    calculateInterest: function(parameters) {
-        return parameters.principal * parameters.rate * parameters.term;
-    }
+function loadLibrary(libraryName) {
+    var script = document.createElement('script');
+    script.src = `library/${libraryName}.js`;
+    script.onload = function() {
+        console.log(`${libraryName} library loaded successfully.`);
+        document.dispatchEvent(new CustomEvent('libraryLoaded', { detail: libraryName }));
+    };
+    script.onerror = function() {
+        console.error(`Failed to load library: ${libraryName}`);
+    };
+    document.head.appendChild(script);
 }
