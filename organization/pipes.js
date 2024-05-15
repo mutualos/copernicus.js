9ff728ffbfbaca862de9d4c52fd12dc275826f47
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-14 21:28:14 -0400

    Create pipes.js
    initial commit

---
+++ organization/pipes.js
@@ -0,0 +1,9 @@
+const translations = {
+    loans: {  //match pipeID
+        "Principal": "principal",
+        "Rate_Over_Split": "annualRate",
+        "Maturity_Date": "maturityDate",
+        "Class_Code": "type"
+        // Additional mappings as necessary
+    }
+};
