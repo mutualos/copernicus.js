b0a52920b2e06e76b37c35cba7902ad8c6fc06a0
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-14 21:26:00 -0400

    Create build.js
    initial commit

---
+++ design/build.js
@@ -0,0 +1,6 @@
+window.buildConfig = {
+    libraries: ['organization', 'financial'], // List the libraries your SPA needs
+    formula: 'calculateInterest + feeIncome / 12', // Default formula (example)
+    version: '1.0.0', // Version of the build
+    // Add other settings as needed
+};
