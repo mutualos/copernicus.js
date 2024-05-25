bdc40cb7d78bbb07ccc4d0b44974b36fd2b60b48
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-25 08:59:51 -0400

    Update loadLibraries.js
    reduced the size of the libraries object to contain only relevant library data

--- core/loadLibraries.js
+++ core/loadLibraries.js
@@ -13,13 +13,25 @@ function loadLibrary(library, callback) {
 
     script.onload = function() {
         console.log(`Library loaded successfully from ${libraryUrl}`);
-        // Combine the loaded library into the libraries object
-        Object.keys(window).forEach(key => {
-            if (!['libraries', 'buildConfig'].includes(key) && typeof window[key] === 'object') {
-                Object.assign(window.libraries, window[key]);
-                delete window[key];
-            }
-        });
+        let libContent;
+
+        // Extract the library content
+        if (libraryUrl.startsWith('http://') || libraryUrl.startsWith('https://')) {
+            libContent = window.api;  // For external libraries, assuming it's always window.api
+            delete window.api; // Clean up the global namespace
+        } else {
+            const libName = libraryUrl.split('/').pop().split('.')[0];
+            libContent = window[libName];
+            delete window[libName]; // Clean up the global namespace
+        }
+
+        // Merge the library content into the combined libraries object if valid
+        if (libContent) {
+            Object.assign(window.libraries, libContent);
+        } else {
+            console.error(`Library content for ${libraryUrl} is undefined or null.`);
+        }
+
         callback(null);
     };
 
@@ -34,8 +46,6 @@ function loadLibrary(library, callback) {
 function loadLibraries(libraries, finalCallback) {
     let loadedCount = 0;
     const totalLibraries = libraries.length;
-
-    // Initialize the combined libraries object
     window.libraries = {};
 
     libraries.forEach(library => {
