28479ccaf522bddf0f3941e9a1ac9cd8ffc808db
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-25 09:05:14 -0400

    Update build.js
    introduction to components and moved primary_keys and sort inside presentation

--- design/build.js
+++ design/build.js
@@ -1,14 +1,25 @@
 window.buildConfig = {
-    libraries: ['organization', 'financial', 'https://fijs.net/api/trates/'], // List the libraries your SPA needs
-    formula: 'calculateInterest + feeIncome / 12', // Default formula (example)
-    version: '1.0.0', // Version of the build
+    libraries: ['organization', 'financial', 'https://fijs.net/api/trates/'],
+    version: '1.0.0',
     presentation: {
         columns: [
-            { header: 'ID', key: 'ID', type: 'integer' }, // Display ID in column 1
-            { header: 'Principal', key: 'principal', type: 'currency' }, // Display principal in column 2
-            { header: 'Result', key: 'result', type: 'float' } // Display result in column 3
-        ]
+            { header: 'ID', key: 'ID', type: 'integer' },
+            { header: 'Principal', key: 'principal', type: 'currency' },
+            { header: 'Result', key: 'result', type: 'float' }
+        ],
+        primary_key: 'ID',
+        sort: { key: 'result', order: 'desc' }
     },
-    primary_key: 'ID',
-    sort: { key: 'result', order: 'desc' }
+    components: [
+        {
+            name: 'LoanComponent',
+            formula: '((annualRate - trates:12)  * averagePrincipal - originationExpense - servicingExpense) * (1 - taxRate) - loanLossReserve',
+            pipeIDs: ['loan', 'lending', 'line']
+        },
+        {
+            name: 'AnotherComponent',
+            formula: 'anotherFormula',
+            pipeIDs: ['another', 'example']
+        }
+    ]
 };
