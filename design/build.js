4ae1a8f8f9986cdc4ef5d1ce279e902fe3213f53
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-26 10:08:49 -0400

    Update build.js
    updates to component design and added new component

--- design/build.js
+++ design/build.js
@@ -5,6 +5,7 @@ window.buildConfig = {
         columns: [
             { header: 'ID', key: 'ID', type: 'integer' },
             { header: 'Principal', key: 'principal', type: 'currency' },
+            { header: 'Balance', key: 'balance', type: 'currency' },
             { header: 'Result', key: 'result', type: 'float' }
         ],
         primary_key: 'ID',
@@ -12,14 +13,14 @@ window.buildConfig = {
     },
     components: [
         {
-            name: 'LoanComponent',
+            id: 'loans',
             formula: '((annualRate - trates:12)  * averagePrincipal - originationExpense - servicingExpense) * (1 - taxRate) - loanLossReserve',
             pipeIDs: ['loan', 'lending', 'line']
         },
         {
-            name: 'AnotherComponent',
-            formula: 'anotherFormula',
-            pipeIDs: ['another', 'example']
+            id: 'checking',
+            formula: 'balance * 0.5',
+            pipeIDs: ['check', 'dda']
         }
     ]
 };
