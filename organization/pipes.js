212c05fc015cc2c73a8f3ff902c9cad0a3846950
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-05-26 10:15:02 -0400

    Update pipes.js
    started the checking build

--- organization/pipes.js
+++ organization/pipes.js
@@ -3,8 +3,14 @@ const translations = {
         "Principal": "principal",
         "Rate_Over_Split": "annualRate",
         "Maturity_Date": "maturityDate",
+        "Risk_Rating": "riskRating",
         "Class_Code": "type",
-        "Portfolio": "ID"
+        "Portfolio": "ID",
+        "Previous_Average_Balance": "balance"
         // Additional mappings as necessary
+    },
+    checking: {
+        "Portfolio": "ID",
+        "Previous_Average_Balance": "balance"
     }
 };
