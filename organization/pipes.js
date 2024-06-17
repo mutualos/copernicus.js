fe9a4e247d62b981f3a1554dfc0d8e8d09eea911
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-06-17 06:48:10 -0400

    Update pipes.js
    updated translations

--- organization/pipes.js
+++ organization/pipes.js
@@ -2,16 +2,28 @@ const translations = {
     loans: {  //match pipeID
         "Principal": "principal",
         "Rate_Over_Split": "annualRate",
+        "Date_Opened": "openDate",
         "Maturity_Date": "maturityDate",
         "Risk_Rating": "riskRating",
         "Class_Code": "type",
         "Portfolio": "ID",
-        "Previous_Average_Balance": "balance"
+        "Branch_Number": "branch",
+        "Opened_by_Resp_Code": "responsibilty"
         // Additional mappings as necessary
     },
     checking: {
         "Portfolio": "ID",
-        "Previous_Average_Balance": "balance"
+        "Class_Code": "type",
+        "Date_Opened": "openDate",
+        "Previous_Average_Balance": "balance",
+        "PMTD_Service_Charge": "charges",
+        "PMTD_Service_Charge_Waived": "chargesWaived",
+        "PMTD_Other_Charges": "otherCharges",
+        "PMTD_Other_Charges_Waived": "otherChargesWaived",
+        "PMTD_Interest_Earned": "interestExpense",
+        "PMTD_Number_of_Deposits": "deposits",
+        "Branch_Number": "branch",
+        "Opened_by_Resp_Code": "responsibilty"
     }
 };
 window.translations = translations;
