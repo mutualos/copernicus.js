9dcc4ac8a97628930e676de1f38cbaecd041b42e
Fi.js <138887864+mutualos@users.noreply.github.com>
2024-07-03 11:43:06 -0400

    Update saveApp.js
    updates

--- builder/saveApp.js
+++ builder/saveApp.js
@@ -1,24 +1,37 @@
 document.getElementById('saveButton').addEventListener('click', function() {
-    const fileName = document.getElementById('fileNameInput').value;
+    const title = document.getElementById('title').value;
     const editorContent = document.getElementById('editor').innerText;
 
-    if (!fileName) {
-        alert('Please enter a file name.');
+    if (!title) {
+        alert('Please enter a Title under Brief.');
         return;
     }
+    fileName = title.split(' ').join('_');
 
-    // Function to identify used pipes in the formula
-    function identifyUsedPipes(content) {
-        const usedPipes = new Set();
+    // Function to identify pipe used in the formula
+    function identifyPipe(formula) {
+        const pipeCount = new Map();
         pipeItems.forEach(pipe => {
             pipe.items.forEach(item => {
-                if (content.includes(item)) {
-                    usedPipes.add(pipe.category);
+                if (formula.includes(item)) {
+                    const count = pipeCount.get(pipe.category) || 0;
+                    pipeCount.set(pipe.category, count + 1);
                 }
             });
         });
-        return Array.from(usedPipes);
+    
+        let mostFrequentCategory = null;
+        let maxCount = 0;
+        pipeCount.forEach((count, category) => {
+            if (count > maxCount) {
+                mostFrequentCategory = category;
+                maxCount = count;
+            }
+        });
+    
+        return mostFrequentCategory;
     }
+
 
     // Create a mapping of functions, attributes, and dictionaries to their respective libraries
     const libraryMapping = {
@@ -104,53 +117,60 @@ document.getElementById('saveButton').addEventListener('click', function() {
             const key = card.querySelector('input[name="key"]').value;
             const type = card.querySelector('select[name="type"]').value;
             
+            let column = { header, key, type };
+            
+            if (type === 'function') {
+                const formula = card.querySelector('textarea[name="functionFormula"]').value;
+                const pipeID = card.querySelector('select[name="pipeID"]').value;
+                column.function = formula;
+                column.pipeID = pipeID;
+            }
+            
             if (header && key && type) {
-                columns.push({ header, key, type });
+                columns.push(column);
             }
         });
     
         const columnsString = columns.map(col => {
-            return `\t\t\t\t\t{ header: '${col.header}', key: '${col.key}', type: '${col.type}' }`;
+            if (col.type === 'function') {
+                return `\t\t\t\t\t{ header: '${col.header}', key: '${col.key}', type: '${col.type}', function: '${col.function}', pipeID: '${col.pipeID}' }`;
+            } else {
+                return `\t\t\t\t\t{ header: '${col.header}', key: '${col.key}', type: '${col.type}' }`;
+            }
         }).join(',\n');
-
+    
         const charts = [];
-            const chartCards = document.querySelectorAll('.chart-card');
-
-            chartCards.forEach(card => {
-                const label = card.querySelector('input[name="chartLabel"]').value;
-                const key = card.querySelector('select[name="chartKey"]').value;
-
-                if (label && key) {
-                    charts.push({ key, label });
-                }
-            });
-
-            const chartsString = charts.map(chart => {
-                return `{ key: '${chart.key}', label: '${chart.label}' }`;
-            }).join(',\n');
+        const chartCards = document.querySelectorAll('.chart-card');
+    
+        chartCards.forEach(card => {
+            const label = card.querySelector('input[name="chartLabel"]').value;
+            const key = card.querySelector('select[name="chartKey"]').value;
+    
+            if (label && key) {
+                charts.push({ key, label });
+            }
+        });
+    
+        const chartsString = charts.map(chart => {
+            return `{ key: '${chart.key}', label: '${chart.label}' }`;
+        }).join(',\n');
     
         const primaryKey = document.getElementById('primaryKey').value;
         const sortKey = document.getElementById('sortKey').value;
         const sortOrder = document.getElementById('sortOrder').value;
     
-        return `columns: [\n${columnsString}\n\t\t\t\t],\n\t\t\t\tprimary_key: '${primaryKey}',\n\t\t\t\tsort: { key: '${sortKey}', order: '${sortOrder}',\n\t\t\t\tcharts: [\n\t\t\t\t\t${chartsString}\n\t\t\t\t] }`;
+        return `columns: [\n${columnsString}\n\t\t\t\t],\n\t\t\t\tprimary_key: '${primaryKey}',\n\t\t\t\tsort: { key: '${sortKey}', order: '${sortOrder}' },\n\t\t\t\tcharts: [\n\t\t\t\t\t${chartsString}\n\t\t\t\t]`;
     }
 
     // Create components based on the formulas
     const components = formulas.map((formula, index) => {
-        const usedPipes = identifyUsedPipes(formula);
-        console.log('usedPipes', index, usedPipes);
-        let componentId = `component_${index}`;
-        
-        if (usedPipes.length > 0) {
-            componentId = usedPipes[index];
-        }
-
-        console.log('usedPipes[index]', index, usedPipes[index], usedPipes)
+        const associatedPipe = identifyPipe(formula);
+        console.log('associatedPipe', index, associatedPipe);
+        //let componentId = `component_${index}`;
         return {
-            id: componentId,
+            id: associatedPipe,
             formula: formula,
-            pipeIDs: populatePipeIDs(usedPipes[0]) 
+            pipeIDs: populatePipeIDs(associatedPipe) 
         };
     });
 
@@ -160,39 +180,82 @@ document.getElementById('saveButton').addEventListener('click', function() {
         identifyLibraries(formula).forEach(lib => libraries.add(lib));
     });
 
+    function renderComponents(components) {
+        const componentsString = components.map(component => {
+            const pipeIDsString = component.pipeIDs.map(pipeID => `'${pipeID}'`).join(', ');
+            return `\t\t\t\t{\n` +
+                   `\t\t\t\t\tid: '${component.id}',\n` +
+                   `\t\t\t\t\tformula: '${component.formula}',\n` +
+                   `\t\t\t\t\tpipeIDs: [${pipeIDsString}]\n` +
+                   `\t\t\t\t}`;
+        }).join(',\n');
+        return `components: [\n${componentsString}\n\t\t\t]`;
+    }
+
+    function renderLibraries(libraries) {
+        const librariesArray = Array.from(libraries);
+        const librariesString = librariesArray.map((library, index) => {
+            return `\t\t\t\t'${library}'${index < librariesArray.length - 1 ? ',' : ''}\n`;
+        }).join('');
+        return librariesString;
+    }
+
     const fileContent = `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Copernicus Single Page Application</title>
+    <title>${document.getElementById("title").value}</title>
     <link rel="stylesheet" href="../styles/main.css">
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
     <div id="spinnerOverlay" class="spinner-overlay">
         <div class="spinner"></div>
     </div>
     <div id="chartContainer"></div>
-    <div id="resultsTableContainer" class="table-container"></div> <!-- Container for results table -->
-    <footer class="fixed-footer">
-        <input type="file" id="csvPipe" accept=".csv" multiple>
-        <button id="run">
-            <img src="../JS_box.png" class="button-icon">
-            Run
-        </button>
-    </footer>
+    <div id="resultsTableContainer" class="table-container"></div>
+    <!-- Modal -->
+    <div id="configModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <img src="../JS_box.png" alt="Logo">
+                <h2 id="modalTitle"></h2>
+                <span class="close">&times;</span>
+            </div>
+            <div class="modal-body">
+                <div class="file-input-wrapper">
+                    <input type="file" id="csvPipe" accept=".csv" multiple>
+                    <span id="fileNames" class="file-names">No file chosen</span>
+                </div>
+                <button id="run">
+                    Run
+                </button>
+                <h5>Version <span id="modalVersion"></span></h5>
+                <h4>Components</h4>
+                <ul class="component-list" id="componentList"></ul>
+            </div>
+        </div>
+    </div>
+    <footer class="fixed-footer"></footer>
     <script>
         window.buildConfig = {
-            libraries: ${JSON.stringify(Array.from(libraries), null, 4)},
+            libraries: [
+${renderLibraries(libraries)}
+            ],
             version: '${document.getElementById("version").value}',
+            title: '${document.getElementById("title").value}',
+            description: '${document.getElementById("description").value}',
+            author: '${document.getElementById("author").value}',
             presentation: {
                 ${generateColumnsString()},
             },
-            components: ${JSON.stringify(components, null, 4)}
+            ${renderComponents(components)}
         };
     </script>
+    <script src="../core/modal.js"></script>
     <script src="../core/loadLibraries.js"></script>
-    <script src="../organization/pipes.js"></script>
+    <script src="../organization/translator.js"></script>
     <script src="../core/main.js"></script>
 </body>
 </html>`;
